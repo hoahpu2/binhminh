8880dfe81ff913ecb94a30bb5c83171410312bc4
thaohkteam <hoacntt.hpu2@gmail.com>
2019-03-03 17:48:00 -0800

    upload admin

---
+++ database/migrations/2019_03_03_232506_create_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Images', function (Blueprint $table) {
+            $table->increments('IM_id');
+            $table->integer('IM_PR_id');
+            // $table->foreign('IM_PR_id')->references('PR_id')->on('Product')->onDelete('cascade');
+            $table->string('IM_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Images');
+    }
+}
---
+++ database/migrations/2019_03_03_232520_create_silder_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSilderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Slider', function (Blueprint $table) {
+            $table->increments('SL_id');
+            $table->string('SL_url');
+            $table->tinyInteger('SL_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Slider');
+    }
+}
