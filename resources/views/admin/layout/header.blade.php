5c11f1c45f9f95b1baf8e148879d261ac1d42e15
thaohkteam <hoacntt.hpu2@gmail.com>
2019-03-07 22:01:08 -0800

    update git commit -m update

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -215,16 +215,16 @@
             <!-- User Account: style can be found in dropdown.less -->
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="{{ url('admin/') }}/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                <span class="hidden-xs">Alexander Pierce</span>
+                <img src="{{url('resources/upload/useradmin/').'/'.Auth::user()->avatar}}" class="user-image" alt="User Image">
+                <span class="hidden-xs">{{Auth::user()->name}}</span>
               </a>
               <ul class="dropdown-menu">
                 <!-- User image -->
                 <li class="user-header">
-                  <img src="{{ url('admin/') }}/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                  <img src="{{url('resources/upload/useradmin/').'/'.Auth::user()->avatar}}" class="img-circle" alt="User Image">
 
                   <p>
-                    Alexander Pierce - Web Developer
+                    {{Auth::user()->name}} - Web Developer
                     <small>Member since Nov. 2012</small>
                   </p>
                 </li>
